a506402f82f387c443756b026bae94e9c0fa58f6
saransh vaid <saransh.vaid@transflo.com>
2021-06-07 14:26:45 -0400

    createProject

---
+++ src/main/java/com/iamatester/App.java
@@ -0,0 +1,13 @@
+package com.iamatester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
