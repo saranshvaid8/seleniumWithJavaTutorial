d1f4f48ea3dd9df99d8b28b1246ac32e0de7b3b9
saransh vaid <saransh.vaid@transflo.com>
2021-06-07 17:10:39 -0400

    adding selenium class

--- src/test/java/com/iamatester/AppTest.java
+++ src/test/java/com/iamatester/AppTest.java
@@ -1,5 +1,6 @@
 package com.iamatester;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,6 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        assertFalse(false);
     }
 }
---
+++ src/test/java/taudemo/SeleniumTest.java
@@ -0,0 +1,34 @@
+package taudemo;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SeleniumTest {
+
+
+    @BeforeAll
+    public void beforeAll() {
+
+    }
+
+    @AfterAll
+    public void afterAll() {
+
+    }
+
+    @Test
+    public void openThePageAndCheckTheTitle() {
+        //Do something
+        assertEquals("", "");
+    }
+
+    @Test
+    public void openTheOrgPageAndCheckTheTitle() {
+        //Do something
+        assertEquals("", "");
+    }
+}
